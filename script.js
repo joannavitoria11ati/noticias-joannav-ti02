5c589d89f1d1bc1ad4e3927ccabdfd972f16ef90
Joanna Vitória <joannanepomuceno07@gmail.com>
2025-06-09 21:22:56 -0300

    Criação da navegação desktop, remoção da classe header-mobile e início da responsividade

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const botaoAbrir = document.querySelector(".header-mobile > button")
-const botaoFechar = querySelector (".header-mobile nav")
+const botaoAbrir = document.querySelector("header > button")
+const botaoFechar = document.querySelector("header nav button")
 
 const body = document.querySelector("body")
-const nav = document.querySelector(".header-mobile nav")
+const nav = document.querySelector("header .mobile")
 
 
 
